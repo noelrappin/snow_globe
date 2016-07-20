71eed4e0bb23257263e852ef8862bdb68736ffea
Noel Rappin <noel@tablexi.com>
2016-07-19 19:16:26 -0500

    tests pass again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: {user: 0, vip: 1, admin: 2}
+
+  has_many :tickets
+
+  ## START: code.user_tickets_in_cart
+  def tickets_in_cart
+    tickets.waiting.all.to_a
+  end
+  ## END: code.user_tickets_in_cart
 
 end
