c3f9d4333738581f51ea15aeb7e665d6f5ad5b06
Noel Rappin <noel@tablexi.com>
2016-07-22 07:11:14 -0500

    controller spec

--- spec/controllers/shopping_cart_controller_spec.rb
+++ spec/controllers/shopping_cart_controller_spec.rb
@@ -4,28 +4,29 @@ describe ShoppingCartsController do
 
   describe "update" do
     let(:user) { instance_spy(User) }
-    let(:inventory) { instance_spy(Inventory, event: "event_path") }
+    let(:performance) { instance_spy(
+        Performance, event: build_stubbed(:event)) }
     let(:action) { instance_spy(AddsToCart) }
 
     before(:example) do
       allow(@controller).to receive(:current_user).and_return(user)
-      allow(Inventory).to receive(:find).with("2").and_return(inventory)
+      allow(Performance).to receive(:find).with("2").and_return(performance)
       allow(AddsToCart).to receive(:new).with(
-          inventory: inventory, count: "1", user: user).and_return(action)
+          performance: performance, count: "1", user: user).and_return(action)
     end
 
     it "calls add to cart on a successful" do
-      allow(action).to receive(:result).and_return(true)
-      patch :update, inventory_id: "2", ticket_count: "1"
+      allow(action).to receive(:success).and_return(true)
+      patch :update, params: {performance_id: "2", ticket_count: "1"}
       expect(action).to have_received(:run)
       expect(@controller).to redirect_to(shopping_cart_path)
     end
 
     it "redirects back to the event if unsuccessful" do
-      allow(action).to receive(:result).and_return(false)
-      patch :update, inventory_id: "2", ticket_count: "1"
+      allow(action).to receive(:success).and_return(false)
+      patch :update, params: {performance_id: "2", ticket_count: "1"}
       expect(action).to have_received(:run)
-      expect(@controller).to redirect_to(inventory.event)
+      expect(@controller).to redirect_to(performance.event)
     end
   end
 
