9f0150373abfa2e7bca5f5e6562caa7f444f76cd
Noel Rappin <noel@tablexi.com>
2016-07-22 19:54:00 -0500

    fixes

--- spec/models/stripe_charge_spec.rb
+++ spec/models/stripe_charge_spec.rb
@@ -6,7 +6,7 @@ describe StripeCharge, :vcr do
       credit_card_number: "4242424242424242", expiration_month: "12",
       expiration_year: Time.zone.now.year + 1, cvc: "123") }
   let(:payment) { build_stubbed(
-      :payment, price: Money.new(3000), reference: "reference") }
+      :payment, price: Money.new(3000), reference: Payment.generate_reference) }
 
   it "calls stripe to get a charge" do
     charge = StripeCharge.charge(token: token, payment: payment)
