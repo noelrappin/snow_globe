223144117b6d068bb51fcdfd413b31121e1336c1
Noel Rappin <noel@tablexi.com>
2016-08-02 21:22:30 -0500

    oops

--- app/workflows/cash_purchases_cart.rb
+++ app/workflows/cash_purchases_cart.rb
@@ -16,7 +16,7 @@ class CashPurchasesCart < PreparesCart
 
   def pre_purchase_valid?
     raise UnauthorizedPurchaseException.new(user: user) unless user.admin?
-    super
+    true
   end
 
   def on_failure
