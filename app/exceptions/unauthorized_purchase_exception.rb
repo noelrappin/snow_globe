fba3e3366da51daac9588a37e35d3c5e190ce2b2
Noel Rappin <noel@tablexi.com>
2016-08-02 21:18:39 -0500

    passing

---
+++ app/exceptions/unauthorized_purchase_exception.rb
@@ -0,0 +1,10 @@
+class UnauthorizedPurchaseException < StandardError
+
+  attr_accessor :message, :user, :expected_purchase_cents, :expected_ticket_ids
+
+  def initialize(message = nil, user:)
+    super(message)
+    @user = user
+  end
+
+end
---
+++ app/workflows/cash_purchases_cart.rb
@@ -0,0 +1,26 @@
+class CashPurchasesCart < PreparesCart
+
+  def update_tickets
+    tickets.each(&:purchased!)
+  end
+
+  def on_success
+    @success = true
+  end
+
+  def payment_attributes
+    super.merge(
+        payment_method: "cash", status: "succeeded",
+        administrator_id: user.id)
+  end
+
+  def pre_purchase_valid?
+    raise UnauthorizedPurchaseException.new(user: user) unless user.admin?
+    super
+  end
+
+  def on_failure
+    unpurchase_tickets
+  end
+
+end
